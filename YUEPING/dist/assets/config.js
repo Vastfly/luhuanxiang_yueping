b5b8fb8b0e727a9bab1f4a0232ce8ae9afab05f6
Vastfly <luhuanxiao830@gmail.com>
2026-06-18 11:51:55 +0800

    Delete YUEPING directory

--- YUEPING/dist/assets/config.js
+++
@@ -1,5 +0,0 @@
-window.REVUE_CONFIG = {
-  supabaseUrl: "https://xaubhdmaamzwdrubcdwb.supabase.co",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhhdWJoZG1hYW16d2RydWJjZHdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2ODI0NjgsImV4cCI6MjA5NzI1ODQ2OH0.zHqWPbKMw1g9S2f0_5qTPlQT4QQM2ydexpSadcA_FEQ",
-  coverBucket: "album-covers"
-};
